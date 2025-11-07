0f53cb8d8418a4a0022c5b520bbbc4c7f6f89720
sandrokhachidze2012-coder · im him <sandrokhachidze2012@gmail.com>
2025-11-07 10:07:34 -0800

    second commit

---
+++ DAY 0/day 1/lesson1.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+#i want to paint a house
+width(10)
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+
+left(90)
+forward(70)
+
+left(90)
+forward(115)
+
+right(90)
+forward(70)
+
+right(90)
+forward(115)
+
+penup()
+goto(200 ,200)
+pendown()
+
+right(150)
+forward(200)
+
+left(120)
+forward(210)
+
+penup()
+goto(40 ,150)
+pendown()
+
+left(35)
+forward(25)
+
+left(90)
+forward(100)
+left(90)
+forward(25)
+left(90)
+forward(100)
+
+exitonclick()
